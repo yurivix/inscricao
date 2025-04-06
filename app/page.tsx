4f6fbf9bba360feb2baa81c1169f38741994e5f0
yurivix <yuri.vix@gmail.com>
2025-04-05 21:27:35 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+import UploadInscricao from "@/components/UploadInscricao";
+
+export default function Home() {
+  return (
+    <main className="p-6">
+      <UploadInscricao />
+    </main>
+  );
+}
